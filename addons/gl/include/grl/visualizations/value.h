d4e32ef0440d0fd9361f43da57c0c70fb8bcf651
Wouter Caarls <wouter@caarls.org>
2015-03-11 16:50:07 -0300

    Forgot to add some files

---
+++ addons/gl/include/grl/visualizations/value.h
@@ -0,0 +1,98 @@
+/** \file value.h
+ * \brief Value function visualization header file.
+ *
+ * \author    Wouter Caarls <wouter@caarls.org>
+ * \date      2015-01-22
+ *
+ * \copyright \verbatim
+ * Copyright (c) 2015, Wouter Caarls
+ * All rights reserved.
+ *
+ * This file is part of GRL, the Generic Reinforcement Learning library.
+ *
+ * GRL is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * \endverbatim
+ */
+
+#ifndef GRL_VALUE_VISUALIZATION_H_
+#define GRL_VALUE_VISUALIZATION_H_
+
+#include <string.h>
+#include <pthread.h>
+
+#include <grl/projector.h>
+#include <grl/representation.h>
+#include <grl/policies/q.h>
+#include <grl/visualizations/field.h>
+
+namespace grl
+{
+
+/// Value function visualization.
+class ValueVisualization : public FieldVisualization
+{
+  public:
+    TYPEINFO("visualization/field/value")
+
+  protected:
+    Projector *projector_;
+    Representation *representation_;
+    size_t dim_;
+  
+  public:
+    ValueVisualization() : projector_(NULL), representation_(NULL), dim_(0) { }
+    ~ValueVisualization()
+    {
+      stopAndJoin();
+    }
+
+    // From Configurable
+    virtual void request(ConfigurationRequest *config);
+    virtual void configure(Configuration &config);
+    virtual void reconfigure(const Configuration &config);
+  
+    // From FieldVisualization
+    virtual double value(const Vector &in) const;
+};
+
+/// Policy-value function visualization.
+class PolicyValueVisualization : public FieldVisualization
+{
+  public:
+    TYPEINFO("visualization/field/policy/value")
+    
+  protected:
+    Projector *projector_;
+    Representation *representation_;
+    QPolicy *policy_;
+  
+  public:
+    PolicyValueVisualization() : projector_(NULL), representation_(NULL), policy_(NULL) { }
+    ~PolicyValueVisualization()
+    {
+      stopAndJoin();
+    }
+
+    // From Configurable
+    virtual void request(ConfigurationRequest *config);
+    virtual void configure(Configuration &config);
+    virtual void reconfigure(const Configuration &config);
+  
+    // From FieldVisualization
+    virtual double value(const Vector &in) const;
+};
+
+}
+
+#endif /* GRL_VALUE_VISUALIZATION_H_ */
---
+++ addons/gl/src/value.cpp
@@ -0,0 +1,109 @@
+/** \file value.cpp
+ * \brief Value function visualization source file.
+ *
+ * \author    Wouter Caarls <wouter@caarls.org>
+ * \date      2015-01-22
+ *
+ * \copyright \verbatim
+ * Copyright (c) 2015, Wouter Caarls
+ * All rights reserved.
+ *
+ * This file is part of GRL, the Generic Reinforcement Learning library.
+ *
+ * GRL is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * \endverbatim
+ */
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+#include <grl/visualizations/value.h>
+
+#define EPS 0.001
+
+using namespace grl;
+
+REGISTER_CONFIGURABLE(ValueVisualization) 
+REGISTER_CONFIGURABLE(PolicyValueVisualization)
+
+void ValueVisualization::request(ConfigurationRequest *config)
+{
+  FieldVisualization::request(config);
+  
+  config->push_back(CRP("output_dim", "Output dimension to visualize", (int)dim_, CRP::Online, 0));
+
+  config->push_back(CRP("projector", "projector", "Projects inputs onto representation space", projector_));
+  config->push_back(CRP("representation", "representation", "Value representation", representation_));
+}
+
+void ValueVisualization::configure(Configuration &config)
+{
+  FieldVisualization::configure(config);
+  
+  dim_ = config["output_dim"];
+  projector_ = (Projector*)config["projector"].ptr();
+  representation_ = (Representation*)config["representation"].ptr();
+  
+  // Create window  
+  create("Value function");
+  
+  // Let's get this show on the road
+  start();
+}
+
+void ValueVisualization::reconfigure(const Configuration &config)
+{
+}
+
+double ValueVisualization::value(const Vector &in) const
+{
+  Vector v;
+  representation_->read(projector_->project(in), &v);
+  return v[dim_];
+}
+
+void PolicyValueVisualization::request(ConfigurationRequest *config)
+{
+  FieldVisualization::request(config);
+
+  config->push_back(CRP("projector", "projector.pair", "Projects observation-action pairs onto representation space", projector_));
+  config->push_back(CRP("representation", "representation.value/action", "Q-value representation", representation_));
+  config->push_back(CRP("policy", "policy/discrete/q", "Q-value based control policy", policy_));
+}
+
+void PolicyValueVisualization::configure(Configuration &config)
+{
+  FieldVisualization::configure(config);
+  
+  projector_ = (Projector*)config["projector"].ptr();
+  representation_ = (Representation*)config["representation"].ptr();
+  policy_ = (QPolicy*)config["policy"].ptr();
+  
+  // Create window  
+  create("Value function");
+  
+  // Let's get this show on the road
+  start();
+}
+
+void PolicyValueVisualization::reconfigure(const Configuration &config)
+{
+}
+
+double PolicyValueVisualization::value(const Vector &in) const
+{
+  Vector action, q;
+  policy_->act(in, &action);
+  return representation_->read(projector_->project(in, action), &q);
+}
