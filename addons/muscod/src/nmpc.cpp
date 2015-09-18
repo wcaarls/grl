9adb09349623165d6a18fe8defc0c221314e4d3a
Manuel Kudruss <manuel.kudruss@iwr.uni-heidelberg.de>
2015-09-18 12:09:32 +0200

    added basic version of MUSCOD without reinitialization

--- addons/muscod/src/nmpc.cpp
+++ addons/muscod/src/nmpc.cpp
@@ -14,12 +14,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * \endverbatim
@@ -51,13 +51,60 @@ void NMPCPolicy::configure(Configuration &config)
   model_path_ = config["model_path"].str();
   model_name_ = config["model_name"].str();
   outputs_ = config["outputs"];
-        
+
   muscod_->setModelPathAndName(model_path_.c_str(), model_name_.c_str());
   muscod_->loadFromDatFile(NULL, NULL);
+  // muscod_->sqpInitialize(muscod_->getSSpec(), NULL, NULL);
+  muscod_->setLogLevelScreen(-1);
+  muscod_->setLogLevelAndFile(-1, NULL, NULL);
+  // muscod_->sqpSolve();
+  // std::cout << "MUSCOD is ready!" << std::endl;
+
+  // get proper dimensions
+  muscod_->nmpcInitialize(muscod_->getSSpec(), NULL, NULL);
+  // solve until convergence to prepare solver
+  for (int ii=0; ii < 20; ++ii)
+  {
+    muscod_->nmpcFeedback(NULL, NULL, NULL);
+    muscod_->nmpcTransition();
+    muscod_->nmpcPrepare();
+  }
+
+  // save solver state
+  data_.backup_muscod_state(muscod_);
+
+  // signal readiness
+  // std::cout << "MUSCOD is ready!" << std::endl;
 }
 
 void NMPCPolicy::reconfigure(const Configuration &config)
 {
+}
+
+
+void NMPCPolicy::muscod_reset()
+{
+  // load solution state
+  data_.restore_muscod_state(muscod_);
+
+  // solve until convergence to prepare solver
+  // muscod_->nmpcInitialize(muscod_->getSSpec(), NULL, NULL);
+  // muscod_->setLogLevelScreen(-1);
+  // muscod_->setLogLevelAndFile(-1, NULL, NULL);
+  // muscod_->nmpcPrepare();
+  // muscod_->options->wflag = MS_WARM;
+  // muscod_->sqpSolve();
+
+  // solve until convergence to prepare solver
+  for (int ii=0; ii < 20; ++ii)
+  {
+    muscod_->nmpcFeedback(NULL, NULL, NULL);
+    muscod_->nmpcTransition();
+    muscod_->nmpcPrepare();
+  }
+
+  // signal readiness
+  // std::cout << "MUSCOD is reseted!" << std::endl;
 }
 
 NMPCPolicy *NMPCPolicy::clone() const
@@ -67,23 +114,22 @@ NMPCPolicy *NMPCPolicy::clone() const
 
 void NMPCPolicy::act(double time, const Vector &in, Vector *out)
 {
-  if (time == 0.)
-    muscod_->nmpcInitialize(muscod_->getSSpec(), NULL, NULL);
+  if (time == 0.0)
+  {
+    // muscod_->nmpcInitialize(muscod_->getSSpec(), NULL, NULL);
+    // muscod_->setLogLevelScreen(-1);
+    // muscod_->setLogLevelAndFile(-1, NULL, NULL);
+    // muscod_->nmpcPrepare();
+    // muscod_->options->wflag = MS_WARM;
+    muscod_reset();
+  }
 
   out->resize(outputs_);
   for (int ii=0; ii < 3; ++ii)
   {
-    muscod_->nmpcFeedback(in.data(), out->data());    
+    muscod_->nmpcFeedback(in.data(), NULL, out->data());
     muscod_->nmpcTransition();
+    muscod_->nmpcShift(3);
     muscod_->nmpcPrepare();
   }
-  
-  DVec pf_lo = muscod_->data.dataMSSQP->vbnd.lo.pf;
-  DVec pf_up = muscod_->data.dataMSSQP->vbnd.up.pf;
-  for (int ii = 0; ii < muscod_->data.dataMSSQP->vdim.pf; ++ii) {
-      V_EACC(pf_up, ii) = time;
-      V_EACC(pf_lo, ii) = time;
-  }
-  scale_pf(pf_lo);
-  scale_pf(pf_up);
 }
