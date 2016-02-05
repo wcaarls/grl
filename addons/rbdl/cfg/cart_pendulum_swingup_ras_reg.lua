65fa00ff329584c5953ec287583dfa6704e0c028
Ivan Koryakovskiy <i.koryakovskiy@tudelft.nl>
2016-02-05 15:59:50 +0100

    Prepared experiment of sse reduction method: potential vs box

--- addons/rbdl/cfg/cart_pendulum_swingup_ras_reg.lua
+++ addons/rbdl/cfg/cart_pendulum_swingup_ras_reg.lua
@@ -29,12 +29,20 @@ end
 
 -- Angle is changed so that it is 0 at swingup, and -pi or pi in the bootom.
 -- This ensures that maximum potential is 0 (at swingup) and minimum is < 0 everywhere else
-function getPotential(state)
+function getPotentialSquared(state)
   angle = wrap_angle(state[1]) - math.pi
   return -2    *  state[0]^2 
          -1    *  angle^2
          -0.2  *  state[2]^2
          -0.5  *  state[3]^2
+end
+
+function getPotentialAbsolute(state)
+  angle = wrap_angle(state[1]) - math.pi
+  return -2    *  abs(state[0]) 
+         -1    *  abs(angle)
+         -0.2  *  abs(state[2])
+         -0.5  *  abs(state[3])
 end
 
 function failed(state)
@@ -109,9 +117,9 @@ function evaluate(state, action, next)
     end
   else
     if not reward_shaping then
-      return getPotential(next)
+      return getPotentialSquared(next)
     else
-      return getPotential(next) - 0.0001*action[0]^2 + shaping_weight*(shaping_gamma * int(succeeded(next)) - int(succeeded(state)))
+      return getPotentialSquared(next) - 0.0001*action[0]^2 + shaping_weight*(shaping_gamma * int(succeeded(next)) - int(succeeded(state)))
     end
   end
 end
