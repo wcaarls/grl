51cc718306270bdb3a248ed29c89617bd8b1589b
Wouter Caarls <wouter@caarls.org>
2016-12-04 20:32:20 -0200

    Updated tests

--- bin/runtests.py
+++ bin/runtests.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
-import os, inspect, tempfile, glob, subprocess, filecmp, shutil
+import sys, os, inspect, tempfile, glob, subprocess, filecmp, shutil
+
+generate = False
+if len(sys.argv) > 1:
+  if sys.argv[1] == "generate":
+    generate = True
 
 binpath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 os.chdir(binpath + '/../tests')
@@ -13,21 +18,25 @@ for test in tests:
   print "Test " + test
   
   subprocess.call([binpath + '/../build/grld','-s','1',binpath + '/../tests/' + test], stdout=devnull)
-
+  
   (root, ext) = os.path.splitext(test)
   
-  for template in glob.glob(binpath + '/../tests/template/' + root + '-*.txt'):
-    result = os.path.basename(template)
-    
-    fc = False
-    try:
-      fc = filecmp.cmp(result, template)
-    except:
-      print "Test " + test + "... failed to produce result."
+  if generate:
+    for template in glob.glob(root + '-*.txt'):
+      shutil.copy(template, binpath + '/../tests/template')
+  else:
+    for template in glob.glob(binpath + '/../tests/template/' + root + '-*.txt'):
+      result = os.path.basename(template)
+      
+      fc = False
+      try:
+        fc = filecmp.cmp(result, template)
+      except:
+        print "Test " + test + "... failed to produce result."
 
-    if not fc:
-      print "Test " + test + "... failed."
-      break
+      if not fc:
+        print "Test " + test + "... failed."
+        break
 
 shutil.rmtree(tempdir)
   
