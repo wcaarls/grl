29451645898b26ad1f6a2617103de8f01a7c2453
Ivan Koryakovskiy <i.koryakovskiy@tudelft.nl>
2017-01-10 17:49:31 +0100

    Removing Ivan's configuration and restoring build options

--- base/build.cmake
+++ base/build.cmake
@@ -7,7 +7,7 @@ add_library(${TARGET} SHARED
             ${SRC}/agents/fixed.cpp
             ${SRC}/agents/black_box.cpp
             ${SRC}/agents/td.cpp
-#            ${SRC}/agents/dyna.cpp
+            ${SRC}/agents/dyna.cpp
             ${SRC}/agents/sequential.cpp
 #            ${SRC}/agents/smdp_master.cpp
             ${SRC}/agents/compartmentalized.cpp
@@ -19,33 +19,33 @@ add_library(${TARGET} SHARED
             ${SRC}/configurable.cpp
             ${SRC}/discretizers/uniform.cpp
             ${SRC}/discretizers/peaked.cpp
-#            ${SRC}/discretizers/split.cpp
+            ${SRC}/discretizers/split.cpp
             ${SRC}/discretizers/policy.cpp
-#            ${SRC}/environments/acrobot.cpp
+            ${SRC}/environments/acrobot.cpp
             ${SRC}/environments/observation.cpp
             ${SRC}/environments/modeled.cpp
             ${SRC}/environments/sandbox.cpp
             ${SRC}/environments/pendulum.cpp
             ${SRC}/environments/cart_pole.cpp
-#            ${SRC}/environments/cart_double_pole.cpp
-#            ${SRC}/environments/pinball.cpp
-#            ${SRC}/environments/windy.cpp
+            ${SRC}/environments/cart_double_pole.cpp
+            ${SRC}/environments/pinball.cpp
+            ${SRC}/environments/windy.cpp
             ${SRC}/environments/compass_walker/SWModel.cpp
             ${SRC}/environments/compass_walker/compass_walker.cpp
-#            ${SRC}/environments/noise.cpp
-#            ${SRC}/environments/swimmer.cpp
-#            ${SRC}/environments/shaping.cpp
-#            ${SRC}/environments/flyer2d.cpp
-#            ${SRC}/experiments/approx_test.cpp
+            ${SRC}/environments/noise.cpp
+            ${SRC}/environments/swimmer.cpp
+            ${SRC}/environments/shaping.cpp
+            ${SRC}/environments/flyer2d.cpp
+            ${SRC}/experiments/approx_test.cpp
             ${SRC}/experiments/online_learning.cpp
             ${SRC}/experiments/batch_learning.cpp
-#            ${SRC}/experiments/rpc_env.cpp
-#            ${SRC}/experiments/multi.cpp
+            ${SRC}/experiments/rpc_env.cpp
+            ${SRC}/experiments/multi.cpp
             ${SRC}/exporters/csv.cpp
             ${SRC}/importers/csv.cpp
             ${SRC}/mappings/multisine.cpp
             ${SRC}/mappings/q_policy.cpp
-#            ${SRC}/optimizers/rwa.cpp
+            ${SRC}/optimizers/rwa.cpp
             ${SRC}/policies/random.cpp
             ${SRC}/policies/action.cpp
             ${SRC}/policies/v.cpp
@@ -54,38 +54,38 @@ add_library(${TARGET} SHARED
             ${SRC}/policies/parameterized.cpp
             ${SRC}/policies/feed_forward.cpp
             ${SRC}/policies/pid.cpp
-#            ${SRC}/policies/mcts.cpp
-#            ${SRC}/policies/ucb.cpp
-#            ${SRC}/policies/state_feedback.cpp
-#            ${SRC}/policies/noise.cpp
+            ${SRC}/policies/mcts.cpp
+            ${SRC}/policies/ucb.cpp
+            ${SRC}/policies/state_feedback.cpp
+            ${SRC}/policies/noise.cpp
             ${SRC}/predictor.cpp
             ${SRC}/predictors/model.cpp
             ${SRC}/predictors/sarsa.cpp
-#            ${SRC}/predictors/ggq.cpp
+            ${SRC}/predictors/ggq.cpp
             ${SRC}/predictors/ac.cpp
             ${SRC}/predictors/fqi.cpp
-#            ${SRC}/predictors/mbfqi.cpp
+            ${SRC}/predictors/mbfqi.cpp
             ${SRC}/predictors/qv.cpp
             ${SRC}/predictors/td.cpp
             ${SRC}/predictors/vi.cpp
             ${SRC}/predictors/advantage.cpp
-#            ${SRC}/predictors/multi.cpp
+            ${SRC}/predictors/multi.cpp
             ${SRC}/predictors/dpg.cpp
-#            ${SRC}/projectors/identity.cpp
+            ${SRC}/projectors/identity.cpp
             ${SRC}/projectors/normalizing.cpp
             ${SRC}/projectors/peaked.cpp
             ${SRC}/projectors/scaling.cpp
             ${SRC}/projectors/tile_coding.cpp
-#            ${SRC}/projectors/fourier.cpp
-#            ${SRC}/projectors/monomial.cpp
-#            ${SRC}/projectors/grid.cpp
-#            ${SRC}/projectors/rbf.cpp
-#            ${SRC}/projectors/multi.cpp
+            ${SRC}/projectors/fourier.cpp
+            ${SRC}/projectors/monomial.cpp
+            ${SRC}/projectors/grid.cpp
+            ${SRC}/projectors/rbf.cpp
+            ${SRC}/projectors/multi.cpp
             ${SRC}/representations/linear.cpp
             ${SRC}/representations/ann.cpp
 #            ${SRC}/representations/dmp.cpp
-#            ${SRC}/representations/additive.cpp
-#            ${SRC}/representations/iterative.cpp
+            ${SRC}/representations/additive.cpp
+            ${SRC}/representations/iterative.cpp
             ${SRC}/samplers/greedy.cpp
             ${SRC}/samplers/softmax.cpp
             ${SRC}/samplers/pada.cpp
@@ -95,15 +95,15 @@ add_library(${TARGET} SHARED
             ${SRC}/solvers/agent.cpp
             ${SRC}/solvers/vi.cpp
             ${SRC}/traces/enumerated.cpp
-#            ${SRC}/visualizations/acrobot.cpp
+            ${SRC}/visualizations/acrobot.cpp
             ${SRC}/visualizations/pendulum.cpp
             ${SRC}/visualizations/cart_pole.cpp
-#            ${SRC}/visualizations/cart_double_pole.cpp
+            ${SRC}/visualizations/cart_double_pole.cpp
             ${SRC}/visualizations/compass_walker.cpp
-#            ${SRC}/visualizations/pinball.cpp
-#            ${SRC}/visualizations/windy.cpp
-#            ${SRC}/visualizations/swimmer.cpp
-#            ${SRC}/visualizations/flyer2d.cpp
+            ${SRC}/visualizations/pinball.cpp
+            ${SRC}/visualizations/windy.cpp
+            ${SRC}/visualizations/swimmer.cpp
+            ${SRC}/visualizations/flyer2d.cpp
            )
 
 # Dependencies
