b973711cf764753d02f728d8ff8815a537927d9b
renata-garcia <32511898+renata-garcia@users.noreply.github.com>
2018-04-07 11:25:36 -0300

    MultiPolicy (#12)
    renata-garcia:
    * Extended MultiPolicy to different combination strategies.
    * Implemented ProbabilityACPredictor update.
    * Fixed some configuration files

--- base/src/policies/multi.cpp
+++ base/src/policies/multi.cpp
@@ -33,7 +33,9 @@ REGISTER_CONFIGURABLE(MultiPolicy)
 
 void MultiPolicy::request(ConfigurationRequest *config)
 {
-  config->push_back(CRP("strategy", "Combination strategy", strategy_str_, CRP::Configuration, {"add_probabilities"}));
+  config->push_back(CRP("strategy", "Combination strategy", strategy_str_, CRP::Configuration, {"policy_strategy_add_prob", "policy_strategy_multiply_prob", "policy_strategy_majority_voting_prob", "policy_strategy_rank_voting_prob"}));
+  //TODO: rgo
+  config->push_back(CRP("tau", "Temperature of Boltzmann distribution", tau_));
   config->push_back(CRP("discretizer", "discretizer.action", "Action discretizer", discretizer_));
   config->push_back(CRP("policy", "mapping/policy", "Sub-policies", &policy_));
 }
@@ -41,11 +43,19 @@ void MultiPolicy::request(ConfigurationRequest *config)
 void MultiPolicy::configure(Configuration &config)
 {
   strategy_str_ = config["strategy"].str();
-  if (strategy_str_ == "add_probabilities")
+  if (strategy_str_ == "policy_strategy_add_prob")
     strategy_ = csAddProbabilities;
+  else if (strategy_str_ == "policy_strategy_multiply_prob")
+    strategy_ = csMultiplyProbabilities;
+  else if (strategy_str_ == "policy_strategy_majority_voting_prob")
+    strategy_ = csMajorityVotingProbabilities;
+  else if (strategy_str_ == "policy_strategy_rank_voting_prob")
+    strategy_ = csRankVotingProbabilities;
   else
     throw bad_param("mapping/policy/multi:strategy");
 
+  tau_ = config["tau"];
+  
   discretizer_ = (Discretizer*)config["discretizer"].ptr();
   
   policy_ = *(ConfigurableList*)config["policy"].ptr();
@@ -95,27 +105,230 @@ void MultiPolicy::act(double time, const Observation &in, Action *out)
 void MultiPolicy::distribution(const Observation &in, const Action &prev, LargeVector *out) const
 {
   LargeVector dist;
+  LargeVector param_choice;
   
-  // Get first policy probabilities
-  policy_[0]->distribution(in, prev, out);
-  
-  for (size_t ii=1; ii != policy_.size(); ++ii)
+  switch (strategy_)
   {
-    // Add subsequent policies' probabilities according to chosen strategy
-    policy_[ii]->distribution(in, prev, &dist);
-    if (dist.size() != out->size())
-    {
-      ERROR("Subpolicy " << ii << " has incompatible number of actions");
-      throw bad_param("mapping/policy/multi:policy");
-    }
-    
-    for (size_t jj=0; jj < dist.size(); ++jj)
-    {
-      switch (strategy_)
+    case csAddProbabilities:
+
+      policy_[0]->distribution(in, prev, out);
+      param_choice = LargeVector::Zero(out->size());
+      
+      for (size_t ii=0; ii != policy_.size(); ++ii)
       {
-        case csAddProbabilities:
-          (*out)[jj] += dist[jj];
+        // Add subsequent policies' probabilities according to chosen strategy
+        policy_[ii]->distribution(in, prev, &dist);
+        if (dist.size() != out->size())
+        {
+          ERROR("Subpolicy " << ii << " has incompatible number of actions");
+          throw bad_param("mapping/policy/multi:policy");
+        }
+
+        for (size_t jj=0; jj < dist.size(); ++jj)
+          param_choice[jj] += dist[jj];
       }
+      
+      CRAWL("MultiPolicy::param_choice: " << param_choice);
+      normalized_function(param_choice, out);
+      CRAWL("MultiPolicy::out: " << (*out) << "\n");
+      break;
+        
+    case csMultiplyProbabilities:
+        
+      policy_[0]->distribution(in, prev, out);
+      param_choice = LargeVector::Ones(out->size());
+
+      for (size_t ii=0; ii != policy_.size(); ++ii)
+      {
+        // Multiply subsequent policies' probabilities according to chosen strategy
+        policy_[ii]->distribution(in, prev, &dist);
+        if (dist.size() != out->size())
+        {
+          ERROR("Subpolicy " << ii << " has incompatible number of actions");
+          throw bad_param("mapping/policy/multi:policy");
+        }
+
+        for (size_t jj=0; jj < dist.size(); ++jj)
+          param_choice[jj] *= dist[jj];
+      }
+      
+      CRAWL("MultiPolicy::param_choice: " << param_choice);
+      normalized_function(param_choice, out);
+      CRAWL("MultiPolicy::out: " << (*out) << "\n");
+      
+      break;
+        
+    case csMajorityVotingProbabilities:
+      
+      policy_[0]->distribution(in, prev, out);
+      param_choice = LargeVector::Zero(out->size());
+        
+      for (size_t ii=0; ii != policy_.size(); ++ii)
+      {
+        policy_[ii]->distribution(in, prev, &dist);
+        if (dist.size() != out->size())
+        {
+          ERROR("Subpolicy " << ii << " has incompatible number of actions");
+          throw bad_param("mapping/policy/multi:policy");
+        }
+                
+        double p_best_ind = 0.0;
+        short i_ind = dist.size() - 1;
+        for (size_t jj=0; jj < dist.size(); ++jj)
+        {
+          if (dist[jj] > p_best_ind)
+          {
+            p_best_ind = dist[jj];
+            i_ind = jj;
+          }
+        }
+        param_choice[i_ind] += 1;
+      }
+      
+      CRAWL("MultiPolicy::param_choice: " << param_choice);
+      softmax(param_choice, out);
+      CRAWL("MultiPolicy::out: " << (*out));
+      
+      break;
+        
+    case csRankVotingProbabilities:
+      
+      LargeVector rank_weights;
+      
+      policy_[0]->distribution(in, prev, out);
+      param_choice = LargeVector::Zero(out->size());
+        
+      for (size_t ii=0; ii != policy_.size(); ++ii)
+      {
+        policy_[ii]->distribution(in, prev, &dist);
+        if (dist.size() != out->size())
+        {
+          ERROR("Subpolicy " << ii << " has incompatible number of actions");
+          throw bad_param("mapping/policy/multi:policy");
+        }
+        
+        CRAWL("MultiPolicy::dist: " << dist);
+        //raking        
+        rank_weights = LargeVector::Ones(out->size());
+        for (size_t jj=0; jj < dist.size(); ++jj)
+        {
+          
+          for (size_t kk = 0; kk < dist.size(); kk++)
+          {
+            if(dist[jj] > dist[kk])
+              ++rank_weights[jj];
+            CRAWL("MultiPolicy::dist[: " << jj << "](" << dist[jj] << ") > dist [" << kk << "](" << dist[kk] << "): rank_weights[" << jj << "]("  << rank_weights[jj] << ")");
+          }
+          CRAWL("MultiPolicy::policy: " << ii);
+          CRAWL("MultiPolicy::rank_weights: " << rank_weights);
+          CRAWL("\nMultiPolicy::param_choice: " << param_choice << '\n');
+          param_choice[jj] += rank_weights[jj];
+          CRAWL("\nMultiPolicy::param_choice: " << param_choice << '\n');
+        }
+      }
+      
+      CRAWL("\nMultiPolicy::param_choice: " << param_choice << '\n');
+      softmax(param_choice, out);
+      CRAWL("MultiPolicy::out: " << (*out));
+      
+      break;
+  }      
+}
+
+void MultiPolicy::softmax(const LargeVector &values, LargeVector *distribution) const
+{
+  LargeVector v = LargeVector::Zero(values.size());
+  for (size_t ii=0; ii < values.size(); ++ii)
+    if (std::isnan(values[ii]))
+      ERROR("SoftmaxSampler: NaN value in Boltzmann distribution 1");
+
+  distribution->resize(values.size());
+  const double threshold = -100;
+
+  // Find max_power and min_power, and center of feasible powers
+  double max_power = -DBL_MAX;
+  for (size_t ii=0; ii < values.size(); ++ii)
+  {
+    double p = values[ii]/tau_;
+    max_power = (max_power < p) ? p : max_power;
+  }
+  double min_power = max_power + threshold;
+  double center = (max_power+min_power)/2.0;
+
+  // Discard powers from interval [0.0; threshold] * max_power
+  double sum = 0;
+  for (size_t ii=0; ii < values.size(); ++ii)
+  {
+    double p = values[ii]/tau_;
+    if (p > min_power)
+    {
+      p -= center;
+      v[ii] = exp(p);
+      sum += v[ii];
+      (*distribution)[ii] = 1;
+
+      if (std::isnan(v[ii]))
+        ERROR("SoftmaxSampler: NaN value in Boltzmann distribution 2");
     }
+    else
+      (*distribution)[ii] = 0;
+  }
+
+  for (size_t ii=0; ii < values.size(); ++ii)
+  {
+    (*distribution)[ii] *= v[ii]/sum;
+    if (std::isnan((*distribution)[ii]))
+      ERROR("SoftmaxSampler: NaN value in Boltzmann distribution 4");
   }
 }
+
+void MultiPolicy::normalized_function(const LargeVector &values, LargeVector *distribution) const
+{
+  
+  
+  LargeVector v = LargeVector::Zero(values.size());
+  for (size_t ii=0; ii < values.size(); ++ii)
+    if (std::isnan(values[ii]))
+      ERROR("OtherSelectionMultiPolicy: NaN value in  distribution 1");
+
+  distribution->resize(values.size());
+  const double threshold = -100;
+
+  // Find max_power and min_power, and center of feasible powers
+  double max_power = -DBL_MAX;
+  for (size_t ii=0; ii < values.size(); ++ii)
+  {
+    double p = 1.0/tau_;
+    max_power = (max_power < p) ? p : max_power;
+  }
+  double min_power = max_power + threshold;
+  double center = (max_power+min_power)/2.0;
+
+  // Discard powers from interval [0.0; threshold] * max_power
+  double sum = 0;
+  for (size_t ii=0; ii < values.size(); ++ii)
+  {
+    double p = 1.0/tau_;
+    if (p > min_power)
+    {
+      p -= center;
+      v[ii] = pow(values[ii], p);
+      sum += v[ii];
+      (*distribution)[ii] = 1;
+
+      if (std::isnan(v[ii])) 
+        ERROR("OtherSelectionMultiPolicy: NaN value in  distribution 2");
+    }
+    else {
+      (*distribution)[ii] = 0;
+    }
+  }
+
+  for (size_t ii=0; ii < values.size(); ++ii)
+  {
+    (*distribution)[ii] *= v[ii]/sum;
+    if (std::isnan((*distribution)[ii]))
+      ERROR("OtherSelectionMultiPolicy: NaN value in  distribution 4");
+  }
+}
+
